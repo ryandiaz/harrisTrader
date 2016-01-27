78d8074b7953d542ff3161dc1fbf33f0a54ac29d
ryandiaz <diazry@gmail.com>
2016-01-26 23:32:18 -0800

    added to data utils, can now get stocks in date range

--- src/dataUtils/loadData.py
+++ src/dataUtils/loadData.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import numpy as np
-import os
 
-DATAROOT = "../../data/"
+from datetime import datetime
+import os
+import pdb
+
+DATAROOT = "/home/ryan/ml/harrisTrader/data/"
 
 def close_prices(symbol):
     filename = DATAROOT + symbol + ".csv"
@@ -17,16 +20,37 @@ def percent_changes(symbol):
 
 def get_list_dates(symbol):
     filename = DATAROOT + symbol + ".csv"
-    history = pd.read_csv(filename)
-    np_data = history.values
-    start = np_data[-1,0]
-    end = np_data[0,0]
-    return (start, end)
+    try:
+        history = pd.read_csv(filename)
+        np_data = history.values
+        start = np_data[-1,0]
+        end = np_data[0,0]
+        return (start, end)
+    except:
+        print "unexpected error on reading data for symbol: ", symbol
+    return ("1000-01-01", "1000-01-01")
+
+def get_all_symbols():
+    return [x[:-4] for x in os.listdir(DATAROOT)]
+
+def symbols_in_int(d1, d2):
+    symbols = get_all_symbols()
+    # naively
+    int_symbols = []
+    for symbol in symbols:
+        dates = get_list_dates(symbol)
+        if stock_date_from_str(dates[0]) <= d1 and stock_date_from_str(dates[1]) >= d2:
+            int_symbols.append(symbol)
+    return int_symbols
+
+def stock_date_from_str(date_str):
+    return datetime.strptime(date_str, "%Y-%m-%d")
 
 def main():
-    symbols = os.listdir(DATAROOT)
-    for symbol in symbols:
-        print symbol
-        print close_prices(symbol)
+    date_start = stock_date_from_str("1995-01-01")
+    date_end = stock_date_from_str("2005-01-01")
+    symbols = symbols_in_int(date_start, date_end)
+    print "stocks in range", len(symbols)
+    pdb.set_trace()
 
 if __name__ == "__main__": main()
